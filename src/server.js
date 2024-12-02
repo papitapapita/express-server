494bdb4cbde3ea2d4e9813e3ef6f6679a96adb08
Jonathan <many.d.r.o.2001@hotmail.com>
2024-12-02 11:19:18 -0500

    cross origin allowed again and vercel configuration

--- src/server.js
+++ src/server.js
@@ -10,23 +10,26 @@ const { dirname } = import.meta;
 const app = express();
 const port = process.env.PORT || 3000;
 
-const whitelist = ['http://localhost:5500'];
+const whitelist = [
+  'http://localhost:5500',
+  'http://localhost:3000'
+];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Origin not allowed'));
+      callback(new Error(`Origin not allowed ${origin}`));
     }
   }
 };
 
-app.use(cors(options));
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(dirname, 'public')));
 app.use(logger);
 
-app.get('/about', (req, res) => {
+app.get('/api/about', (req, res) => {
   res.sendFile(path.join(dirname, 'public', 'about.html'));
 });
 
